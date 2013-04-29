fc1376aa60d6643c84659389ca01d8ea1dd8a4cc
adam <adam.vincze@adesso.de>
2013-04-29 17:08:06 +0200

    verbose flag added, openn in browser added o Please enter the commit message for your changes. Lines starting

--- wsmonitor/src/srv/main.go
+++ wsmonitor/src/srv/main.go
@@ -1,42 +1,44 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"net/http"
 	"code.google.com/p/go.net/websocket"
-	"time"
-	_"strconv"
+	"flag"
+	"log"
+	"fmt"
 	"math/rand"
-	"runtime"
+	"net/http"
 	"os/exec"
+	"runtime"
+	"strconv"
+	"time"
 )
 
 var port *int = flag.Int("p", 23456, "Port to listen.")
 var tick *time.Duration = flag.Duration("tick", time.Second, "Tick")
+var openBrowser *bool = flag.Bool("open", false, "open browser")
+var verbose *bool = flag.Bool("v", false, "verbose")
 
 type hub struct {
 	connections map[*websocket.Conn]bool
-	name string
+	name        string
 }
 
 var hubs map[string]*hub
 
-func (h hub) register(conn *websocket.Conn){
-	h.connections[conn] = true;
+func (h hub) register(conn *websocket.Conn) {
+	h.connections[conn] = true
 }
 
-func (h hub) unregister(conn *websocket.Conn){
-	delete(h.connections, conn);
+func (h hub) unregister(conn *websocket.Conn) {
+	delete(h.connections, conn)
 }
 
-
-func (h hub) broadcast(message string){
+func (h hub) broadcast(message string) {
 	var err error
-	for ws := range h.connections{
+	for ws := range h.connections {
 		err = websocket.Message.Send(ws, message)
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err)
 			break
 		}
 	}
@@ -45,76 +47,79 @@ func (h hub) broadcast(message string){
 func wsServer(ws *websocket.Conn) {
 	hub := hubs["time"] //TODO extract from ws config
 	hub.register(ws)
-	fmt.Println("registered for ws")
+	if *verbose {
+		log.Println("registered for ws")
+	}
+
 	for {
 		var buf string
 		err := websocket.Message.Receive(ws, &buf)
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err)
 			break
 		}
-		time.Sleep(100*time.Millisecond)
+		time.Sleep(100 * time.Millisecond)
 	}
 	hub.unregister(ws)
-	fmt.Println("unregistered for ws")
+	if *verbose {
+		log.Println("unregistered for ws")
+	}
 }
 
 func tickAndDo(fn func(time.Time), d time.Duration) {
-	ticker := time.NewTicker(d);
+	ticker := time.NewTicker(d)
 	for now := range ticker.C {
 		fn(now)
-		fmt.Println("tick", now.Format("15:04:05"))
+		if *verbose {
+			log.Println("tick", now.Format("15:04:05"))
+		}
 	}
 }
 
 type Event struct {
-	TimeStamp int64
-	IntValue int64
+	TimeStamp   int64
+	IntValue    int64
 	StringValue string
 }
 
 func main() {
 	flag.Parse()
-
-
-	
-	
-
-	hubs = map[string]*hub {
-		"time" : &hub{connections:make(map[*websocket.Conn]bool)},
+	hubs = map[string]*hub{
+		"time": &hub{connections: make(map[*websocket.Conn]bool)},
 	}
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	go tickAndDo(func(now time.Time){
-			for ws := range hubs["time"].connections{
-				event := &Event{TimeStamp:now.Unix(), IntValue:r.Int63n(200)}
-				websocket.JSON.Send(ws, *event)
-			}
-		},*tick)
+	go tickAndDo(func(now time.Time) {
+		for ws := range hubs["time"].connections {
+			event := &Event{TimeStamp: now.Unix(), IntValue: r.Int63n(200)}
+			websocket.JSON.Send(ws, *event)
+		}
+	}, *tick)
 
 	http.Handle("/ws", websocket.Handler(wsServer))
 	http.Handle("/", http.FileServer(http.Dir("static")))
-	openBrowser("http://localhost:23456/")
+	if *openBrowser {
+		openUrlInBrowser("http://localhost:" + strconv.Itoa(*port) + "/index.html")
+	}
 	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 	if err != nil {
 		panic("ListenANdServe: " + err.Error())
 	}
 
-	
 }
 
-func openBrowser(url string) {
+func openUrlInBrowser(url string) {
 	var err error
 	switch runtime.GOOS {
 	case "linux":
-	    err = exec.Command("xdg-open", url).Start()
+		err = exec.Command("xdg-open", url).Start()
 	case "windows":
 		err = exec.Command(`C:\Windows\System32\rundll32.exe`, "url.dll,FileProtocolHandler", url).Start()
 	case "darwin":
-	    err = exec.Command("open", url).Start()
+		err = exec.Command("open", url).Start()
 	default:
-	    err = fmt.Errorf("unsupported platform")
+		log.Print("unsupported platform")
 	}
-	if err !=nil {
-		 panic(err)
+	if err != nil {
+		panic(err)
 	}
 }
